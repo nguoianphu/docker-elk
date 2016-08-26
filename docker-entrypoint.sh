ac9ad9e786d4631d8e16229b1dcc57d2e0533935
Nguoi An Phu <nguoianphu@gmail.com>
2016-08-26 15:47:06 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,12 @@ set -e
 # Run as user "elk"
 if [[ "$1" == "" ]]; then
     echo "Starting elasticsearch"
-    set -- gosu elk elasticsearch
+    gosu elk elasticsearch
     sleep 20
     echo "Starting logstash"
-    set -- gosu elk logstash
+    gosu elk logstash
     echo "Starting kibana"
-    set -- gosu elk kibana
+    gosu elk kibana
 else
     # Else allow the user to run arbitrarily commands like bash
     exec "$@"
