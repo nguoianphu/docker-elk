03604c681b4d0a8d05effc0aa050c2ba2b019458
Nguoi An Phu <nguoianphu@gmail.com>
2016-08-26 14:37:54 +0700

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# If user don't provide any command
+# start the ELK server
+# Run as user "elk"
+if [[ "$1" == -* ]]; then
+    set -- gosu elk elasticsearch
+    sleep 20
+    set -- gosu elk logstash
+    set -- gosu elk kibana
+else
+    # Else allow the user to run arbitrarily commands like bash
+    exec "$@"
+fi
