f237e8dd1f003376e4d69c2115f4de38c29dddc4
Nguoi An Phu <nguoianphu@gmail.com>
2016-10-11 15:37:02 +0700

    Test Elasticsearch startup cmd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,8 @@ set -e
 if [[ "$1" == "" ]]; then
     echo "Starting elasticsearch"
     #exec gosu elk elasticsearch -Des.network.host=0.0.0.0 &
-    exec gosu elk elasticsearch -d -p /opt/elasticsearch/elasticsearch.pid -Ees.network.host=0.0.0.0
+    # exec gosu elk elasticsearch -d -p /opt/elasticsearch/elasticsearch.pid -Ees.network.host=0.0.0.0
+    exec gosu elk elasticsearch -d -p /opt/elasticsearch/elasticsearch.pid
     # kill `cat /opt/elasticsearch/elasticsearch.pid`
     sleep 60
     echo "Starting logstash"
